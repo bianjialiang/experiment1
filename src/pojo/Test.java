d57aff364003985fc0224458e029636170f01e1a
bianjialiang <286616839@qq.com>
2018-06-21 14:07:06 +0800

    第二次提交

--- src/pojo/Test.java
+++ src/pojo/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		Student student = new Student();
 		student.setName("卞家良");
 		student.setGrade(3);
+		System.out.println("好好学习，天天向上");
 		student.setAge(27);
 		student.setAddress("安徽省亳州市");
 		System.out.println(student.getAddress());
